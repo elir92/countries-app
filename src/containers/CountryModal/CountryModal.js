48d6ef6f04c5b6b5888940db80c702e417a8f0df
Eli <elirahamim5@gmail.com>
2018-07-16 13:55:00 +0300

    handle with uknown details

--- src/containers/CountryModal/CountryModal.js
+++ src/containers/CountryModal/CountryModal.js
@@ -7,25 +7,25 @@ import './CountryModal.css';
 class CountryModal extends React.Component {
 
     render() {
+        const { modal, toggleHandler, currentCountry } = this.props;
+
         return (
-            <div>
-                <Modal isOpen={this.props.modal} toggle={this.props.toggleHandler} className="country-modal">
-                    <ModalHeader toggle={this.props.toggleHandler}>{this.props.currentCountry.name}</ModalHeader>
-                    <ModalBody>
-                        <Card>
-                            <CardImg src={this.props.currentCountry.flag} alt="Card image cap" className="flag-img" />
-                            <CardBody>
-                                <CardTitle className="card-title">Located in {this.props.currentCountry.region}</CardTitle>
-                                <CardText>
-                                    The capital city is {this.props.currentCountry.capital}. <br />
-                                    {this.props.currentCountry.name} population number is {this.props.currentCountry.population}.  <br />
-                                    {this.props.currentCountry.name} Alpha 3 Code is {this.props.currentCountry.alpha3Code}.
+            <Modal isOpen={modal} toggle={toggleHandler} className="country-modal">
+                <ModalHeader toggle={toggleHandler}>{currentCountry.name}</ModalHeader>
+                <ModalBody>
+                    <Card>
+                        <CardImg src={currentCountry.flag} alt="Card image cap" className="flag-img" />
+                        <CardBody>
+                            <CardTitle className="card-title">Located in {currentCountry.region !== "" ? currentCountry.region : `unknown`}</CardTitle>
+                            <CardText>
+                                The capital city is {currentCountry.capital !== "" ? currentCountry.capital : `unknown`}. <br />
+                                {currentCountry.name} population number is {currentCountry.population}.  <br />
+                                {currentCountry.name} Alpha 3 Code is {currentCountry.alpha3Code}.
                                 </CardText>
-                            </CardBody>
-                        </Card>
-                    </ModalBody>
-                </Modal>
-            </div>
+                        </CardBody>
+                    </Card>
+                </ModalBody>
+            </Modal>
         );
     }
 }
