69b60f5094b9cf4d0d71d81a4aac046a324423bb
Eli <elirahamim5@gmail.com>
2018-09-05 17:50:08 +0300

    Added logos and title to home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './Home.css';
 import globe from '../../globe.svg';
+import reactLogo from '../../react-logo.svg';
+import reduxLogo from '../../redux-logo.svg';
 
-class Home extends Component{
-    render(){
-        return(
+class Home extends Component {
+    render() {
+        return (
             <div className="Home">
                 <h1 className="Home-title">Welcome</h1>
                 <img src={globe} className="Home-logo" alt="globe" />
+                <hr />
+                <h4 className="details__headtitle">Sample application based on</h4>
+                <img className="react-logo" src={reactLogo} alt="logo" />
+                <img className="redux-logo" src={reduxLogo} alt="logo" />
             </div>
         );
     }
