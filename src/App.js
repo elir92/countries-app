3844c2ee2bce86be7ddf010e39ccfcbeff80b8fd
Eli <elirahamim5@gmail.com>
2018-09-05 19:46:33 +0300

    Added switch to the router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'reactstrap';
 import Layout from './containers/Layout';
 import AppNavbar from './components/AppNavbar/AppNavbar';
@@ -14,9 +14,11 @@ class App extends Component {
         <Layout>
           <AppNavbar />
           <Container>
-            <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
-            <Route path={process.env.PUBLIC_URL + "/table"} component={CountriesTable} />
-            <Route path={process.env.PUBLIC_URL + "/flag-game"} component={FlagGame} />
+            <Switch>
+              <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
+              <Route path={process.env.PUBLIC_URL + "/table"} component={CountriesTable} />
+              <Route path={process.env.PUBLIC_URL + "/flag-game"} component={FlagGame} />
+            </Switch>
           </Container>
         </Layout>
       </Router>
