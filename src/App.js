29e01406fdc6d372d18f0a1640e163f194a2c080
Eli <elirahamim5@gmail.com>
2018-09-06 21:04:18 +0300

    Added lazy loadings for the routes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Container } from 'reactstrap';
 import Layout from './containers/Layout';
 import AppNavbar from './components/AppNavbar/AppNavbar';
 import Home from './containers/Home/Home';
-import CountriesTable from './containers/CountriesTable/CountriesTable';
-import FlagGame from './containers/FlagGame/FlagGame';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncTable = asyncComponent(()=> import('./containers/CountriesTable/CountriesTable'));
+const asyncFlagGame = asyncComponent(() => import('./containers/FlagGame/FlagGame'));
 
 class App extends Component {
   render() {
@@ -16,8 +18,8 @@ class App extends Component {
           <Container>
             <Switch>
               <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
-              <Route path={process.env.PUBLIC_URL + "/table"} component={CountriesTable} />
-              <Route path={process.env.PUBLIC_URL + "/flag-game"} component={FlagGame} />
+              <Route path={process.env.PUBLIC_URL + "/table"} component={asyncTable} />
+              <Route path={process.env.PUBLIC_URL + "/flag-game"} component={asyncFlagGame} />
             </Switch>
           </Container>
         </Layout>
