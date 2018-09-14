f46bfe6f0358c75ffeaa38fc549f05767bd778fa
Eli <elirahamim5@gmail.com>
2018-09-14 15:46:01 +0300

    Added game reducer

--- src/store/reducers/game.js
+++ src/store/reducers/game.js
@@ -1,3 +1,10 @@
 import * as actionType from '../actions/actionTypes';
 import { updateObject } from '../../utility';
 
+const initialState = {
+    isPending: false,
+    countries: [],
+    error: null
+}
+
+
