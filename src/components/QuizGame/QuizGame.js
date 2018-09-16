d5ca175ba8c1d798b22773de3a55efd7f5e46dd1
Eli <elirahamim5@gmail.com>
2018-09-16 11:18:39 +0300

    Added stage avatar, changed some css for mobile and desktop

--- src/components/QuizGame/QuizGame.js
+++ src/components/QuizGame/QuizGame.js
@@ -12,18 +12,22 @@ class QuizGame extends Component {
 
         const randFlag = game[currentStage][Math.floor(Math.random() * game[currentStage].length)];
         const answersList = game[currentStage].map(country => {
-            return <li onClick={() => this.answerHandler(country.name, randFlag.name)} key={country.alpha3Code}>{country.name}</li>;
+            return <li key={country.alpha3Code}>
+                <Button onClick={() => this.answerHandler(country.name, randFlag.name)} color="secondary" block>{country.name}</Button>
+            </li>;
         });
 
         return (
             <Row>
-                <Col sm="12" md="6">
+                <Col sm="12" md="6" className="Flag-Col">
                     <img className="Flag-Img" src={randFlag.flag} alt="flag" />
                 </Col>
-                <Col sm="12" md="6">
+                <Col sm="12" md="6" className="Answer-Col">
+                    <div className="Stage-Avatar"><span>{currentStage + 1}</span></div>
                     <ul>
                         {answersList}
                     </ul>
+
                 </Col>
             </Row>
         );
@@ -44,7 +48,7 @@ class QuizGame extends Component {
             <Fragment>
                 {this.props.currentStage !== 10 ? this.renderGame() : <h1>DONE!</h1>}
                 <div className="Restart-Button">
-                    <Button onClick={this.props.restartGameHandler} color="danger" size="lg">Restart Game</Button>
+                    <Button onClick={this.props.restartGameHandler} outline color="secondary" size="lg">Restart Game</Button>
                 </div>
             </Fragment>
         );
