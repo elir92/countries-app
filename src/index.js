34218dcd78173159d36d5a17c6c24a9aeac5ca02
Eli <elirahamim5@gmail.com>
2018-10-02 19:49:36 +0300

    Removed FW CDN. installed the FW package

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createLogger } from 'redux-logger';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
 import App from './App';
 import rootReducer from './store/reducers/rootReducer';
