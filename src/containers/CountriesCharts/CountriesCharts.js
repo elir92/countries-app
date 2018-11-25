e8dd5d378cce81fde111d6226e05f82052669f93
Eli <elirahamim5@gmail.com>
2018-11-25 20:15:01 +0200

    Implemented chart js example

--- src/containers/CountriesCharts/CountriesCharts.js
+++ src/containers/CountriesCharts/CountriesCharts.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { requestCountries } from '../../store/actions/actions';
+import { Pie } from 'react-chartjs-2';
+
+const PopulationGraph = () => {
+    const data = {
+        labels: [
+            'Red',
+            'Green',
+            'Yellow'
+        ],
+        datasets: [{
+            data: [300, 50, 100],
+            backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+            ],
+            hoverBackgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+            ]
+        }]
+    };
+    return <Pie data={data} />
+}
 
 class CountriesCharts extends Component {
+
+
+    componentDidMount() {
+        if (!this.props.countries.length) {
+            this.props.requestCountriesHandler();
+        }
+    }
+
     render() {
         return (
-            <div>Under constructions....</div>
+            <PopulationGraph />
         );
     }
 }
 
-export default CountriesCharts;
+const mapStateToProps = state => {
+    return {
+        countries: state.tableReducer.countries
+    }
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        requestCountriesHandler: () => dispatch(requestCountries())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CountriesCharts);
